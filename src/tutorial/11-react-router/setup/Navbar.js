85f1b831cdebf55cc963c3a2af20009cf1813af7
Idris Yakub <olayinkayakub@yahoo.com>
2022-05-15 03:06:36 +0100

    on it

--- src/tutorial/11-react-router/setup/Navbar.js
+++ src/tutorial/11-react-router/setup/Navbar.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 const Navbar = () => {
-  return <nav>navbar</nav>;
+  return (
+  <nav>
+    <ul>
+      <li>
+        <Link to='/'>
+      </li>
+    </ul>
+  </nav>
+  );
 };
 
 export default Navbar;
